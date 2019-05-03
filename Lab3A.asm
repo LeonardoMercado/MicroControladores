8dcf41b44cf3abd50dc9cca9dee8a9df6ddbf3d8
LeonardoMercado <lfmercadobe@unal.edu.co>
2019-05-02 21:52:02 -0500

    v1.3
    Se implementa el Debugger de la etiqueta antirrebote.

--- Lab3A.asm
+++ Lab3A.asm
@@ -16,6 +16,7 @@
 ;
 ;* Rev #      Date           Who             Comments
 ;* ----- ------------ ------------------- ---------------------------------------------------
+;* 1.3	 02-may-2019 Leonardo M. Benpitez Se implementa el debugger de la etiqueta antirrebote
 ;* 1.2   24-Apr-2019 Leonardo M. Benítez  Se implementa antirebote por software para cada switch.
 ;* 1.1   23-Apr-2019 Maria A. Leonardo    Se implementa modificaciones  al código y queda 
 ;*										  disponible para su primera prueba de funcionamiento.
@@ -24,92 +25,99 @@
 
 ; Include derivative-specific definitions
             INCLUDE 'derivative.inc'
-            
 
 ; export symbols
             XDEF _Startup, main
             ; we export both '_Startup' and 'main' as symbols. Either can
             ; be referenced in the linker .prm file or from C/C++ later on
-            
-            
-            
             XREF __SEG_END_SSTACK   ; symbol defined by the linker for the end of the stack
 
-
 ; variable/data section
-MY_ZEROPAGE: SECTION  SHORT         ; Insert here your data definition
-
-btnst0:   dc.b  1					;Definición de variables de 1 byte 
-btnst1:   dc.b  1					;Definición de variables de 1 byte
-btnst2:   dc.b  1					;Definición de variables de 1 byte
-
+MY_ZEROPAGE: SECTION  SHORT         ;seccion de la pagina zero de la RAM
+btnst0:   dc.b  1                    ;variable para estado de boton
+btnst1:   dc.b  1 
+btnst2:   dc.b  1 
 ; code section
 MyCode:     SECTION
 main:
 _Startup:
-            LDHX   #__SEG_END_SSTACK ;Inicializa los punteros H y X   DIRECCIONAMIENO(IMM)
-            TXS					;inicializa el puntero del stack DIRECCIONAMIENO(INH)
-            mov	   #$f8,PTADD   ;PUERTO A| BIT 0,1,2  COMO INPUT DIRECCIONAMIENO(IMM/DIR)
-            lda	   #$07			;CARGA HEX 07 EN EL ACULUMADOR   DIRECCIONAMIENO(IMM)
-            					;7 6 5 4 3 2 1 0
-            					;0 0 0 0 0 1 1 1
-            sta    PTAPE 		;CONFIGURA PULL UP EN EL PUERTO A BIT 0,1,2      DIRECCIONAMIENO(DIR)
-            mov    #$01,btnst0	;INICIALIZACIÓN DEL ESTADO DEL SWITCH 0 EN ALTO  DIRECCIONAMIENO(IMM/DIR)
-            mov    #$01,btnst1  ;INICIALIZACIÓN DEL ESTADO DEL SWITCH 1 EN ALTO  DIRECCIONAMIENO(IMM/DIR)
-            mov    #$01,btnst2  ;INICIALIZACIÓN DEL ESTADO DEL SWITCH 2 EN ALTO  DIRECCIONAMIENO(IMM/DIR)
-            mov    #$0e,PTBDD	;PUERTO B| BIT 1,2,3  COMO OUTPUT                DIRECCIONAMIENO(IMM/DIR)
-            mov    #$0e,PTBD	;PUERTO B| BIT 1,2,3  EN ALTO                    DIRECCIONAMIENO(IMM/DIR)   
-			
-
-mainLoop:						;ETIQUETA QUE SE EJECUTA EN LOOP
+            LDHX   #__SEG_END_SSTACK ; initialize the stack pointer
+            TXS
+            lda   #$02              ;desactivar watchdog, dejar modo BKGD enabled
+            sta   SOPT1             ;BKGD jamas disabled
+            mov   #%11111000,PTADD  ;puerto A bit 0, 1 y 2 como input
+            lda   #$07              ;cargar hex 07 Bin 0000|0111 en acumulador
+            sta   PTAPE             ;habilitar pull-up en pin 0, 1 y 2 de PTA
+                                    ;puerto en estado natural en 1 logico
+                                    ;boton presionado lo pone en 0 logico
+            mov   #$01,btnst0       ;dar un estado inicial a variable de boton0
+            mov   #$01,btnst1       ;dar un estado inicial a variable de boton1
+            mov   #$01,btnst2       ;dar un estado inicial a variable de boton2
+            mov   #%00000111,PTBDD  ;puerto B bit 0, 1 y 2 como output
+            mov   #%00000111,PTBD   ;bit 0, 1 y 2 del puerto B en 1 logico
             
-            brclr  0,PTAD,boton0 ;PREGUNTA SI EL BIT 0 DEL PUERTO A ESTA EN BAJO, SI EVALUA A VERDAD PASA A LA RUTINA BOTON0 DIRECCIONAMIENO(DIR)
-            brclr  1,PTAD,boton1 ;PREGUNTA SI EL BIT 1 DEL PUERTO A ESTA EN BAJO, SI EVALUA A VERDAD PASA A LA RUTINA BOTON1 DIRECCIONAMIENO(DIR)
-            brclr  2,PTAD,boton2 ;PREGUNTA SI EL BIT 2 DEL PUERTO A ESTA EN BAJO, SI EVALUA A VERDAD PASA A LA RUTINA BOTON2 DIRECCIONAMIENO(DIR)
-            feed_watchdog		 ;LLAMADA A MAMÁ PARA QUE NO REINICIE LA RUTINA.
-            BRA    mainLoop		 ;RETORNA A LA RUTINA MAINLOOP  DIRECCIONAMIENO(REL)
+mainLoop:   
+            brclr 0,PTAD,boton0     ;pregunta si boton esta presionado, osea bit en 0, 
+                                    ;dado el caso salta a rutina boton
+            brclr 1,PTAD,boton1
             
+            brclr 2,PTAD,boton2    
+                                    
+            BRA    mainLoop
             
-;_____________________________________________________________________________________________________________________________ 
-;RUTINA PARA COMPLEMENTO DEL ESTADO SALIDA DEL LED EN EL PUERTO B BIT 1           
+;_________________________________________________________________________________________________________________________
+;*RUTINA DE ANTIREBOTE PARA EL BOTON0            
 boton0:
-			mov   #$00,btnst0  	 ;REFRESCA EL ESTADO DE LA VARIABLE BTNST0  DIRECCIONAMIENO(IMM/DIR)		
-			lda    PTBD			 ;CARGA EN EL ACUMULADOR EL RESGISTRO PTBD(ALMACENA EL ESTADO DEL PUERTO DE SALIDA)  DIRECCIONAMIENO(DIR)
-			eor    #%00000010	 ;OPERACIÓN XOR SOBRE LO QUE HAYA EN EL ACUMULADOR(COMPLEMENTA EL ESTADO DEL PUESTO B BIT 1) DIRECCIONAMIENO(IMM)	
-			sta    PTBD          ;CARGA EL ACUMULADOR EN EL REGISTRO DE PTBD(ACTUALIZA EL ESTADO DEL PUERTO DE SALIDA)  DIRECCIONAMIENO(DIR)
-still0:							 ;INICIO DEL ANTIREBORE SW
-            lda    PTAD          ;CARGA EN EL ACUMULADOR EL RESGISTRO PTAD(ALMACENA EL ESTADO DEL PUERTO DE ENTRADA)  DIRECCIONAMIENO(DIR)
-            cbeq   btnst0,still0 ;COMPARA EL ESTADO DEL BTN0 CON EL ACUMULADOR Y SALTA A STILL0 SI SON IGUALES.  DIRECCIONAMIENO(DIR)
-            nop					 ;SIN OPERACIÓN (PEQUEÑO RETARDO) DIRECCIONAMIENO(INH)                         
-			bra    mainLoop 	 ;RETORNA A LA RUTINA MAINLOOP DIRECCIONAMIENO(REL)
-;_____________________________________________________________________________________________________________________________
+            mov   #%00000110,btnst0       ;atualizar variable estado boton.
+            lda   PTBD              ;cargar estado actual del puerto B en acumulador.
+            eor   #%00000001        ;operación XOR entre el acumulador y el numero indicado
+                                    ;hacer esta operación con ese bit en especifico resulta en alternar el bit
+                                    ;correspondiente, resultado se guarda en el acumulador.
+            sta   PTBD              ;guardar en PTBD lo presente en el acumulador
+still0:
+            lda   PTAD              ;cargar el estado actual del puerto A
+            cbeq  btnst0,still0       ;compare el acumulador con btnst, branch if equal a etiqueta still
+                                    ;haciendo esta comparacion le decimos a el MCU que espere a que dejemos de
+                                    ;pulsar el boton para continuar y asi completar la acción de alternar el estado
+                                    ;del puerto o del LED en este caso
+            nop                     ;no operation para tener una pequqeña pausa
+            bra   mainLoop          ;devuelta al mainLoop.
+;_________________________________________________________________________________________________________________________
 
-
-;_____________________________________________________________________________________________________________________________
-;RUTINA PARA COMPLEMENTO DEL ESTADO SALIDA DEL LED EN EL PUERTO B BIT 2
+;_________________________________________________________________________________________________________________________
+;*RUTINA DE ANTIREBOTE PARA EL BOTON1            
 boton1:
-			mov   #$00,btnst1  	 ;REFRESCA EL ESTADO DE LA VARIABLE BTNST1  DIRECCIONAMIENO(IMM/DIR) 	
-			lda    PTBD          ;CARGA EN EL ACUMULADOR EL RESGISTRO PTBD(ALMACENA EL ESTADO DEL PUERTO DE SALIDA)  DIRECCIONAMIENO(DIR)
-			eor    #%00000100	 ;OPERACIÓN XOR SOBRE LO QUE HAYA EN EL ACUMULADOR(COMPLEMENTA EL ESTADO DEL PUESTO B BIT 2) DIRECCIONAMIENO(IMM)	
-			sta    PTBD			 ;CARGA EL ACUMULADOR EN EL REGISTRO DE PTBD(ACTUALIZA EL ESTADO DEL PUERTO DE SALIDA)  DIRECCIONAMIENO(DIR)
-still1:							 ;INICIO DEL ANTIREBORE SW
-            lda   PTAD           ;CARGA EN EL ACUMULADOR EL RESGISTRO PTAD(ALMACENA EL ESTADO DEL PUERTO DE ENTRADA)  DIRECCIONAMIENO(DIR)
-            cbeq  btnst1,still1  ;COMPARA EL ESTADO DEL BTN1 CON EL ACUMULADOR Y SALTA A STILL1 SI SON IGUALES.  DIRECCIONAMIENO(DIR)
-                                  
-            nop					 ;SIN OPERACIÓN (PEQUEÑO RETARDO) DIRECCIONAMIENO(INH)           
-			bra    mainLoop 	 ;RETORNA A LA RUTINA MAINLOOP DIRECCIONAMIENO(REL)
-			
-;_____________________________________________________________________________________________________________________________
-;RUTINA PARA COMPLEMENTO DEL ESTADO SALIDA DEL LED EN EL PUERTO B BIT 3
+            mov   #%00000101,btnst1        ;atualizar variable estado boton.
+            lda   PTBD              ;cargar estado actual del puerto B en acumulador.
+            eor   #%00000010        ;operación XOR entre el acumulador y el numero indicado
+                                    ;hacer esta operación con ese bit en especifico resulta en alternar el bit
+                                    ;correspondiente, resultado se guarda en el acumulador.
+            sta   PTBD              ;guardar en PTBD lo presente en el acumulador
+still1:
+            lda   PTAD              ;cargar el estado actual del puerto A
+            cbeq  btnst1,still1     ;compare el acumulador con btnst, branch if equal a etiqueta still
+                                    ;haciendo esta comparacion le decimos a el MCU que espere a que dejemos de
+                                    ;pulsar el boton para continuar y asi completar la acción de alternar el estado
+                                    ;del puerto o del LED en este caso
+            nop                     ;no operation para tener una pequqeña pausa
+            bra   mainLoop          ;devuelta al mainLoop.
+;_________________________________________________________________________________________________________________________
 
+;_________________________________________________________________________________________________________________________
+;*RUTINA DE ANTIREBOTE PARA EL BOTON2            
 boton2:
-			mov   #$00,btnst2  	 ;REFRESCA EL ESTADO DE LA VARIABLE BTNST1  DIRECCIONAMIENO(IMM/DIR) 	
-			lda    PTBD          ;CARGA EN EL ACUMULADOR EL RESGISTRO PTBD(ALMACENA EL ESTADO DEL PUERTO DE SALIDA)  DIRECCIONAMIENO(DIR)
-			eor    #%00001000	 ;OPERACIÓN XOR SOBRE LO QUE HAYA EN EL ACUMULADOR(COMPLEMENTA EL ESTADO DEL PUESTO B BIT 3) DIRECCIONAMIENO(IMM)		
-			sta    PTBD			 ;CARGA EL ACUMULADOR EN EL REGISTRO DE PTBD(ACTUALIZA EL ESTADO DEL PUERTO DE SALIDA)  DIRECCIONAMIENO(DIR)
-still2:							 ;INICIO DEL ANTIREBORE SW
-            lda   PTAD           ;CARGA EN EL ACUMULADOR EL RESGISTRO PTAD(ALMACENA EL ESTADO DEL PUERTO DE ENTRADA)  DIRECCIONAMIENO(DIR)
-            cbeq  btnst2,still2  ;COMPARA EL ESTADO DEL BTN2 CON EL ACUMULADOR Y SALTA A STILL2 SI SON IGUALES.  DIRECCIONAMIENO(DIR)
-            nop					 ;SIN OPERACIÓN (PEQUEÑO RETARDO) DIRECCIONAMIENO(INH)                                              
-			bra    mainLoop 	 ;RETORNA A LA RUTINA MAINLOOP DIRECCIONAMIENO(REL)
-;_____________________________________________________________________________________________________________________________
+            mov   #%00000011,btnst2        ;atualizar variable estado boton.
+            lda   PTBD              ;cargar estado actual del puerto B en acumulador.
+            eor   #%00000100        ;operación XOR entre el acumulador y el numero indicado
+                                    ;hacer esta operación con ese bit en especifico resulta en alternar el bit
+                                    ;correspondiente, resultado se guarda en el acumulador.
+            sta   PTBD              ;guardar en PTBD lo presente en el acumulador
+still2:
+            lda   PTAD              ;cargar el estado actual del puerto A
+            cbeq  btnst2,still2       ;compare el acumulador con btnst, branch if equal a etiqueta still
+                                    ;haciendo esta comparacion le decimos a el MCU que espere a que dejemos de
+                                    ;pulsar el boton para continuar y asi completar la acción de alternar el estado
+                                    ;del puerto o del LED en este caso
+            nop                     ;no operation para tener una pequqeña pausa
+            bra   mainLoop          ;devuelta al mainLoop.
+;_________________________________________________________________________________________________________________________
