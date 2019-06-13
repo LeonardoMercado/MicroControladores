f5c0e960cf2071a13a2a260e8d662f9b6c8e0374
Daniel Diaz <49424369+dafedico@users.noreply.github.com>
2019-06-12 21:47:14 -0500

    Version 1.1
    Prueba del ADC (funcionando) y configuraciones iniciales, Enviar datos bloquea el micro

--- Laboratorio8/main.c
+++ Laboratorio8/main.c
@@ -23,8 +23,8 @@
 ;****************************************************************************
 */
 
-#include <hidef.h> /* for EnableInterrupts macro */
-#include "derivative.h" /* include peripheral declarations */
+#include <hidef.h>          /* for EnableInterrupts macro */
+#include "derivative.h"     /* include peripheral declarations */
 
 // Configuraciones Iniciales
 
@@ -38,10 +38,11 @@
 #define Entrada5        APCTL1_ADPC5      // Define la entrada analoga 5 - ADP0 (Pin #)
 #define Entrada6        APCTL1_ADPC6      // Define la entrada analoga 6 - ADP0 (Pin #)
 #define Entrada7        APCTL1_ADPC7      // Define la entrada analoga 7 - ADP0 (Pin #)
+
 #define Entradas        APCTL1          // Permite seleccionar la entrada activa del ADC a convertir, mediante Corrimiento
 
-#define bufferADCH           ADCRH      // Toma el nibble inferior para usarse como los bits 12-8 del valor ADC
-#define bufferADCL           ADCRL      // Toma el valor como los bits 7-0 del valor ADC
+#define bufferADCH      ADCRH           // Toma el nibble inferior para usarse como los bits 12-8 del valor ADC
+#define bufferADCL      ADCRL           // Toma el valor como los bits 7-0 del valor ADC
 
 #define convActiva      ADCSC2_ADACT    // Bandera Conversion Activa, no se puede cambiar de canal si este valor es 1
 #define convCompleta    ADCSC1_COCO     // Bandera de "Conversion Completa"
@@ -53,15 +54,23 @@
 
 // Configuraciones recicladas del Lab. 7
 
-#define display1  PTBD_PTBD0          // Por convención y por orden, se toman los 4 pines del puerto 
-#define display2  PTBD_PTBD1          // ubicados en la tarjeta  para facilitar
-#define display3  PTBD_PTBD2          // el ensamble en la protoboard, desde esta definicion se pueden ubicar
-#define display4  PTBD_PTBD3          // en otro puerto distinto
+#define display1  PTBD_PTBD4          // Por convención y por orden, se toman los 4 pines del puerto 
+#define display2  PTBD_PTBD5          // ubicados en la tarjeta  para facilitar
+#define display3  PTBD_PTBD6          // el ensamble en la protoboard, desde esta definicion se pueden ubicar
+#define display4  PTBD_PTBD7          // en otro puerto distinto
+
+#define enviar    PTDD_PTDD5
+#define cambiar   PTDD_PTDD6  
 
 #define datoDisplay     PTDD        // dato para enviar al display (PTA0-PTA3) (32-35)
 #define bufferSCI       SCI2D       // Buffer de datos del SCI
 
-//variables en RAM
+// variables en RAM
+
+int datoADC[8];                      // Almacena el dato resultante de ADC
+int entrada;
+
+//RAM - Reciclado del Lab7
 
 unsigned char centima, decima, segundo, dsegundo;   //Variables usadas para el numero visto en el display
 unsigned char cronometro;                           //Determina si el cronometro es ascendente (1) o descendente (0)
@@ -76,33 +85,31 @@ volatile unsigned char contDato = 2;
 
 unsigned const time=10;             //Constante en ROM que determina el numero de ciclos del retardo
 
+// Prototipos de Funciones
+
+void setDisplay(unsigned int);      //prototipos de las funciones para que el compilador
+void retardo(unsigned short);       //entienda el nombre y los parametros que reciben/retornan
+void enviarTodo(void);              //
+
+
 void setPrograma()                  // Inicializacion del Programa
 {
     SOPT1 = 0x02;                   // deshabilitar el watchdog
     
-    ADCSC2_ACFE=0;                  // deshabilita la comparacion de valores en ADC
-    ADCSC1_ADCO=0;                  // deshabilita la conversion continua, asi tomara dato a dato que se requiera y activara las banderas
-    ADCCFG_MODE=1;                  // Habilita la conversion de 12 bits
-    APCTL1 = 0x00;                  // Habilita todos los pines de entrada ADCP0-ADCP7
-    
-    PTADD = 0x0f;                   //configuracion de pines para manejo del display 7 segmentos
-    PTBDD_PTBDD0 = 1;               //pin para anodo 1/digito1
-    PTBDD_PTBDD1 = 1;               //pin para anodo 2/digito2
-    PTBDD_PTBDD2 = 1;               //pin para anodo 3/digito3
-    PTBDD_PTBDD3 = 1;               //pin para anodo 4/digito4
-    PTCDD_PTCDD0 = 1;               //define salida para pin 20, PTC0
-    PTCDD_PTCDD1 = 1;               //define salida para pin 19, PTC1
-    PTCDD_PTCDD2 = 1;               //define salida para pin 15, PTC2
+    PTDDD = 0x0f;                   // configuracion de pines para manejo del display 7 segmentos, Puerto D
+    PTBDD = 0xf0;                   // configuracion de pines para manejo del display, seleccion de digito  
 
     dsegundo = 2;                   // Inicializa con un valor predefinido, en este caso se coloco
     segundo = 0;                    // el año actual (2019) para ser visualizado
     decima = 1;                     // como prueba de que el boton reset esta funcionando correctamente
-    centima = 9;                    //
+    centima = 9;
+    
+    display=6666;
     
     EnableInterrupts;               //Macro para habilitar interrupciones
 }
 
-// Reciclado del Lab7
+// Configuracion KBI + Interrupcion KBI
 
 void setKBI()
 {
@@ -113,26 +120,29 @@ void setKBI()
     KBI2SC_KBIMOD = 0;          // Habilita la operacion solo por flancos
     KBI2SC_KBACK = 0;           // Limpia la bandera de Ejecucion de KBI2 
     
-    KBI2PE_KBIPE5 = 1;          // Habilita el Pin 0 de KBI2 (pin 16)
-    KBI2PE_KBIPE6 = 1;          // Habilita el pin 1 de KBI2 (pin 17)
+    KBI2PE_KBIPE5 = 1;          // Habilita KBI2P5 (pin 18)
+    KBI2PE_KBIPE6 = 1;          // Habilita KBI2P6 (pin 17)
     KBI2ES_KBEDG5 = 0;          // Habilita interrupción por flanco de bajada en la entrada KBI2P0
     KBI2ES_KBEDG6 = 0;          // Habilita interrupción por flanco de bajada en ls entrada KBI2P1
 }
-
 interrupt VectorNumber_Vkeyboard void KBI_ISR()
 {
     KBI2SC_KBACK = 1;           // Limpia la bandera de Interrupción
     
+    //Boton Enviar
+    if(enviar==0)
+    {
+        display++;
+    }
     
-    
-    //display=displayTop*100+displayDown;
-    
-    //displayTop=display/100;
-    //displayDown=display%100;
-    
-    //SCI_send(144);              // IMPORTANTE : Dato de Envio
+    //Boton Cambiar
+    if(cambiar==0)
+    {
+        display--;
+    }
 }
 
+// Configuracion SCI + Interrupcion SCI
 
 void setSCI()
 {
@@ -143,7 +153,6 @@ void setSCI()
     SCI2C2_RE = 1;
     SCI2C2_RIE = 1;
 }
-
 void SCI_send(char dato)
 {
     while(SCI2S1_TDRE == 0);
@@ -151,31 +160,104 @@ void SCI_send(char dato)
     return;
 }
 
-interrupt VectorNumber_Vadc void servicioADC(int entrada)
+// Configuracion ADC + Interrupcion ADC
+
+void setADC()
 {
-    Entradas=0;
-    switch(entrada)
+    SCGC1_ADC = 1;                  // Asegura que el modulo ADC reciba señal de reloj del BUSCLK
+    
+    ADCSC1_AIEN=1;                  // Habilita/Deshabilita la conversion ADC
+    ADCSC1_ADCO=0;                  // deshabilita la conversion continua, asi tomara dato a dato que se requiera y activara las banderas
+    ADCSC1_ADCH=0x1f;               // Activa/Desactiva la conversion ADC, 11111 la desactiva, otro valor cambia el canal (0-7 en este caso)
+
+    ADCSC2_ADTRG=0;                 // Establece el disparador del ADC, 0 software, 1 hardware
+    ADCSC2_ACFE=0;                  // deshabilita la comparacion de valores en ADC
+    ADCSC2_ACFGT=0;                 // selecciona el disparo de comp. por hardware, 0 menor que, 1 mayor que
+    
+    ADCCFG_ADLPC=0;                 // Modo Ahorro de energia, 0 deshabilitado
+    ADCCFG_ADIV=0;                  // Divisor de reloj de entrada, 00 sin division, 11 div. 8
+    ADCCFG_MODE=1;                  // Habilita la conversion de 12 bits
+    ADCCFG_ADICLK=0;                // Selecciona el reloj de uso del ADC, 00=Busclk, 01=busclk/2
+    
+    APCTL1 = 0x00;                  // Habilita todos los pines de entrada ADCP0-ADCP7
+    APCTL2 = 0x00;                  // Habilita todos los pines de entrada ADCP9-ADCP15
+}
+interrupt VectorNumber_Vadc void servicioADC()
+{
+    //Entradas=0;
+    switch(canalEntrada)
     {
         case 0:
-            Entrada0=1;
-            
+            //Entrada0=1;
+            datoADC[0]=bufferADCH;
+            datoADC[0]<<8;
+            datoADC[0]=datoADC[0]+bufferADCL;
+        break;
+        case 1:
+            //Entrada1=1;
+            datoADC[1]=bufferADCH;
+            datoADC[1]<<8;
+            datoADC[1]=datoADC[1]+bufferADCL;
+        break;
+        case 2:
+            //Entrada2=1;
+            datoADC[2]=bufferADCH;
+            datoADC[2]<<8;
+            datoADC[2]=datoADC[2]+bufferADCL;
+        break;
+        case 3:
+            //Entrada3=1;
+            datoADC[3]=bufferADCH;
+            datoADC[3]<<8;
+            datoADC[3]=datoADC[3]+bufferADCL;
+        break;
+        case 4:
+            //Entrada4=1;
+            datoADC[4]=bufferADCH;
+            datoADC[4]<<8;
+            datoADC[4]=datoADC[4]+bufferADCL;
+        break;
+        case 5:
+            //Entrada5=1;
+            datoADC[5]=bufferADCH;
+            datoADC[5]<<8;
+            datoADC[5]=datoADC[3]+bufferADCL;
+        break;
+        case 6:
+            //Entrada6=1;
+            datoADC[6]=bufferADCH;
+            datoADC[6]<<8;
+            datoADC[6]=datoADC[6]+bufferADCL;
+        break;
+        case 7:
+            //Entrada7=1;
+            datoADC[7]=bufferADCH;
+            datoADC[7]<<8;
+            datoADC[7]=datoADC[7]+bufferADCL;
         break;
     }
+    Entradas=0;
 }
 
 void main(void)
 {
-  
     setPrograma();                  // Inicializa las variables y parametros del programa
-    EnableInterrupts;
-  /* include your code here */
-
-  
-
-  for(;;) {
-    __RESET_WATCHDOG();	/* feeds the dog */
-  } /* loop forever */
-  /* please make sure that you never leave main */
+    setADC();                       // Inicializa ADC
+    setKBI();                       // Inicializa KBI
+    setSCI();                       // Inicializa SCI
+   
+    for (;;) 
+    {
+        // Prueba ADC
+        
+        canalEntrada=2;
+        Entrada2=1;
+        
+        display=datoADC[2];
+        
+        setDisplay(display);                    //llamar a display constantemente
+        //enviarTodo();
+    } 
 }
 
 // Subrutina que envia sucesivamente todos los datos del programa
@@ -188,14 +270,16 @@ void enviarTodo()
     {
         canalEntrada++;
         SCI_send(bufferADCH);
-        pausa(time);
+        retardo(time);
         SCI_send(bufferADCL);
         Entradas<<1;
     }
     Entradas=0;
 }
 
-void setDisplay(unsigned int var)                   // Reciclado del Ejemplo
+// Reciclado del Lab7
+
+void setDisplay(unsigned int var)                   
 {
     centima=var%10;                //centesimas de s
     decima=var/10%10;             //decimas de s
@@ -204,24 +288,23 @@ void setDisplay(unsigned int var)                   // Reciclado del Ejemplo
     
     datoDisplay = dsegundo;         //cargar numero del digito 1 en PTA
     display1 = 1;                 //encender el digito 1 con el anodo 1
-    pausa(time);              //llamar funcion retardo pasandole valor time para uso en ciclo for
+    retardo(time);              //llamar funcion retardo pasandole valor time para uso en ciclo for
     display1 = 0;                 //apagar el digito 1 para pasar al siguiente
     datoDisplay = segundo;
     display2 = 1;
-    pausa(time);
+    retardo(time);
     display2 = 0;
     datoDisplay = decima;
     display3 = 1;
-    pausa(time);
-    display4 = 0;
+    retardo(time);
+    display3 = 0;
     datoDisplay = centima;
     display4 = 1;
-    pausa(time);
+    retardo(time);
     display4 = 0;
 }
 
-//funcion de retardo por software o polling
-void pausa(unsigned short max)
+void retardo(unsigned short max)
 {
     unsigned volatile int ciclo;
     for (ciclo = 0; ciclo < max; ciclo++) 
